36a68ddbb5635a396577ce32488130e513af7ad6
cubarco <marco.l.here@gmail.com>
2019-04-17 20:07:01 +0800

    Set now as default branch and migrate.

--- purge-cache.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-curl -X DELETE "https://api.cloudflare.com/client/v4/zones/$CLOUDFLARE_ZONE_ID/purge_cache" \
--H "X-Auth-Email: marco.l.here@gmail.com" \
--H "X-Auth-Key: $CLOUDFLARE_API_KEY" \
--H "Content-Type: application/json" \
---data '{"purge_everything":true}'
