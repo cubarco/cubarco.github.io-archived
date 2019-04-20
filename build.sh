cc459decbffe702e7fd492bc2a4cee227336f40c
cubarco <marco.l.here@gmail.com>
2019-04-20 16:24:00 +0800

    Fix now.sh build.

--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@ yum install ruby23-devel.x86_64 libxslt-devel libxml2-devel && \
     export NOKOGIRI_USE_SYSTEM_LIBRARIES=true && \
     export BUNDLE_GEMFILE=$PWD/Gemfile && \
     ruby --version && bundle --version && gem --version && \
-    bundle install --jobs=3 --retry=3 --deployment && \
+    bundle install --jobs=3 --retry=3 && \
     JEKYLL_ENV=production bundle exec jekyll build
