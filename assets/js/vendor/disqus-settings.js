f05661fe354507d2b40c6217162817e178e7f278
cubarco <marco.l.here@gmail.com>
2021-05-21 14:10:29 +0800

    DisqusJS blocks gist-async.

--- assets/js/vendor/disqus-settings.js
+++ assets/js/vendor/disqus-settings.js
@@ -1,8 +1,11 @@
-var dsqjs = new DisqusJS({
-    shortname: window.disqus_shortname,
-    siteName: window.disqus_siteName,
-    api: window.disqus_api,
-    apikey: window.disqus_apikey,
-    admin: window.disqus_admin,
-    adminLabel: window.disqus_adminLabel
-});
+// DisqusJS blocks gist-async
+setTimeout(function() {
+    var dsqjs = new DisqusJS({
+        shortname: window.disqus_shortname,
+        siteName: window.disqus_siteName,
+        api: window.disqus_api,
+        apikey: window.disqus_apikey,
+        admin: window.disqus_admin,
+        adminLabel: window.disqus_adminLabel
+    });
+}, 0)
