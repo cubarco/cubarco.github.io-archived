de0d92ab7b0b2b36a98b4b91952ea23b5253bc07
cubarco <marco.l.here@gmail.com>
2021-05-21 18:50:43 +0800

    Fix.

--- _plugins/hooks/purgecss.rb
+++
@@ -1,3 +0,0 @@
-Jekyll::Hooks.register(:site, :post_write) do |_site|
-  system("./node_modules/.bin/purgecss --css #{_site.dest}/assets/css/main.css --content #{_site.dest}/**/*.html  --output #{_site.dest}/assets/css")
-end
