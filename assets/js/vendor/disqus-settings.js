dbafb626c3e59ba35dd64f247558bf39d6563ea5
cubarco <cubarco@users.noreply.github.com>
2021-05-21 04:59:33 +0000

    jekyll build from Action 36fe09f8b47520f9ded49f032c4f615c96651765

---
+++ assets/js/vendor/disqus-settings.js
@@ -0,0 +1,8 @@
+var dsqjs = new DisqusJS({
+    shortname: window.disqus_shortname,
+    siteName: window.disqus_siteName,
+    api: window.disqus_api,
+    apikey: window.disqus_apikey,
+    admin: window.disqus_admin,
+    adminLabel: window.disqus_adminLabel
+});
