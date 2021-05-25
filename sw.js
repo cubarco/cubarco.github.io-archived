1bc71fd089abfe3d442c7a217366ae13fc9df833
cubarco <marco.l.here@gmail.com>
2021-05-25 11:13:20 +0800

    Use regex to match target url.

--- sw.js
+++ sw.js
@@ -3,22 +3,19 @@
 var CDN_URL = "{{ site.ghmirror }}"
 
 var urlPrefix = self.registration.scope.replace(/\/$/, '');
+var ruleRegex = new RegExp('^' + urlPrefix + '[^#]*/(#[^/]*)*$')
 
 self.addEventListener("fetch", function (event) {
   if (
     event.request.method == "GET" &&
-    event.request.url.startsWith(urlPrefix) &&
-    (event.request.url.endsWith("/") ||
-      event.request.url.split("#")[0].endsWith("/"))
+      ruleRegex.test(event.request.url)
   ) {
     event.respondWith(cdnNetwork(event.request));
   } else return fetch(event.request);
 });
 
 function cdnNetwork(req) {
-  var cdnUrl = req.url.replace(urlPrefix, CDN_URL);
-  cdnUrl = cdnUrl.split("#")[0];
-  cdnUrl += "index.html";
+  var cdnUrl = req.url.replace(urlPrefix, CDN_URL).split('#')[0] + 'index.html'
   return fetch(cdnUrl).then((response) => {
     if (!response.ok) {
       return fetch('/404.html')
