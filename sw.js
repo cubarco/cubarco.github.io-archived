ae89935189e429f1f1e54f62a728d67618930545
cubarco <cubarco@users.noreply.github.com>
2021-05-25 10:28:25 +0000

    jekyll build from Action cd4c7c2e734ff6d83a53949bc6c86a70c7bbd0fc

---
+++ sw.js
@@ -0,0 +1,29 @@
+var CDN_URL = "https://cdn.jsdelivr.net/gh/cubarco/cubarco.github.io@2021-0525-102823-8056"
+
+var urlPrefix = self.registration.scope.replace(/\/$/, '');
+var ruleRegex = new RegExp('^' + urlPrefix + '[^#]*/(#[^/]*)*$')
+
+self.addEventListener("fetch", function (event) {
+  if (
+    event.request.method == "GET" &&
+      ruleRegex.test(event.request.url)
+  ) {
+    event.respondWith(cdnNetwork(event.request));
+  } else return fetch(event.request);
+});
+
+function cdnNetwork(req) {
+  var cdnUrl = req.url.replace(urlPrefix, CDN_URL).split('#')[0] + 'index.html'
+  return fetch(cdnUrl).then((response) => {
+    if (!response.ok) {
+      return fetch('/404.html')
+    }
+    var newHeaders = new Headers(response.headers);
+    newHeaders.set("Content-Type", "text/html");
+    return new Response(response.body, {
+      status: response.status,
+      statusText: response.statusText,
+      headers: newHeaders,
+    });
+  });
+}
